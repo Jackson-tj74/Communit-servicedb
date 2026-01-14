dc9169ec2e5b1f5299b958cdcadc4805aa21dcdd
munyentwali augustin <eagleaust456@gmail.com>
2026-01-14 13:40:25 +0200

    codes

---
+++ src/controller/userController.js
@@ -0,0 +1,33 @@
+import User from "../model/userModel.js"
+
+class Controller{
+
+    static signup=async(req,res)=>{
+
+        const{names,email,password,role}= req.body
+        
+        try{
+
+             const user = await User.create({names,email,password,role})
+
+
+        if(!user){
+
+            return res.status(404).json({message:"Creation of user failed"})
+        }else{
+            return res.status(201).json({message:"User succefully created",user})
+        }
+    }catch (error){
+        console.log(error)
+        return res.status(500).json({message:"create user failed"})
+    }
+
+
+
+    }
+
+
+}
+
+
+export default Controller
