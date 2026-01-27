03fa824a654b60b5fa2d84b69d682ee6ca529e8d
Jackson-tj74 <tuyikundejackson74@gmail.com>
2026-01-27 09:22:31 +0200

    added codes

--- src/routes/bookingRouter.js
+++ src/routes/bookingRouter.js
@@ -4,6 +4,15 @@ import { VerifyAcess } from "../midleware/verifyAcess.js"
 
 
 const router = express.Router()
-router.post("/bookingService",VerifyAcess('client'),BookingController.Booing)
+router.post("/bookingService",VerifyAcess('provider'),BookingController.Booking)
+router.get("/getAllBookings",BookingController.getAllBookings)
+
+router.delete("/deleteAllBookings",BookingController.deleteAllBookings)
+
+router.delete("/deleteOneBooking/:id",BookingController.deleteOneBooking)
+
+
+router.get("getOneBooking/:id",BookingController.getOneBookig)
+router.put("/changeStatus/:id",BookingController.changeStatus)
 
 export default router
